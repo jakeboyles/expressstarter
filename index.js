29abb4ba91698e7d1d6f346f4174f7d519a90d40
Jake Boyles <jake@jibdesigns.com>
2017-04-17 11:57:50 -0400

    initial

--- index.js
+++ index.js
@@ -1,56 +1,30 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-var port = process.env.PORT || 8080;
+const data = [];
 
-
-const data = [
-	{id:0,name:'jake',age:25,pets:100},
-	{id:1,name:'george',age:100,pets:200},
-	{id:2,name:'taco',age:1,pets:4}
-]
-
-
-// Get all people localhost:3001/people
+// Get all of people
 app.get('/people',function(req,res){
-	res.json(data);
 })
 
-
-// Get a single person: localhost:3001/people/10
+// Get a single person
 app.get('/people/:id',function(req,res){
-
-	let id = req.params.id;
-	const single = data.filter(person=>{
-		if(person.id==id) return person;
-	})
-	res.json(single);
 })
 
-
-// Post a new person: localhost:3001/people
+// Post a new person
 app.post('/people',function(req,res){
-	let person = {name:req.body.name,id:req.body.id,pets:req.body.pets};
-	data.push(person);
-	res.json(data)
 })
 
-// Delete a person localhost:3001/people/10 
+// Delete a person
 app.delete('/people/:id',function(req,res){
-	let id = req.params.id;
-	data.forEach((person,index)=>{
-		if(person.id==id){
-			data.splice(index,1);
-		}
-	})
-	res.json(data);
 })
 
 app.listen(port, function() {
-  console.log('Listening on port 3001');
+  console.log(`Listening on port ${port}`);
 });
