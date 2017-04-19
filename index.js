db751b006c43f016245bd1ba5cd444d66fb1d737
Jake Boyles <jake@jibdesigns.com>
2017-04-19 11:51:35 -0400

    adding in mongoose

--- index.js
+++ index.js
@@ -2,29 +2,36 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 8080;
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/user');
 
+mongoose.connect('PUT YOUR MLAB STUFF HERE');
+
+app.use(cors());
 app.use(express.static('public'));
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-const data = [];
 
 // Get all of people
-app.get('/people',function(req,res){
+app.get('/users',function(req,res){
+	User.find().exec((err,response)=>{
+	})
 })
 
-// Get a single person
-app.get('/people/:id',function(req,res){
+app.post('/users',function(req,res){
+	let user = new User();
 })
 
-// Post a new person
-app.post('/people',function(req,res){
+app.get('/users/:id',function(req,res){
 })
 
-// Delete a person
-app.delete('/people/:id',function(req,res){
+app.delete('/users/:id',function(req,res){
 })
 
+
+// Start the server
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
